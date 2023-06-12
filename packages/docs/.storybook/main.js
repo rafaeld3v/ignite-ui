b1da3c941eb949d167d0c66fb399f16532b5368f
Rafael Tavares <rafaelcontato.ce@gmail.com>
2023-06-12 11:21:57 -0300

    publication and maintenance

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -25,7 +25,7 @@ const config = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/ignite-ui";
+      config.base = "/ignite-ui/";
     }
 
     return config;
